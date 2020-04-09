44a3930f7f508c50d9dc997a867010d7f732661f
Dongbin Na <ndb796@naver.com>
2020-04-10 04:12:47 +0900

    Update interpolated_adversarial_training.py

--- interpolated_adversarial_training.py
+++ interpolated_adversarial_training.py
@@ -134,7 +134,6 @@ def test(epoch):
     net.eval()
     benign_loss = 0
     adv_loss = 0
-    correct = 0
     benign_correct = 0
     adv_correct = 0
     total = 0
@@ -148,7 +147,6 @@ def test(epoch):
             benign_loss += loss.item()
 
             _, predicted = outputs.max(1)
-            correct += predicted.eq(targets).sum().item()
             benign_correct += predicted.eq(targets).sum().item()
 
             if batch_idx % 10 == 0:
@@ -162,7 +160,6 @@ def test(epoch):
             adv_loss += loss.item()
 
             _, predicted = adv_outputs.max(1)
-            correct += predicted.eq(targets).sum().item()
             adv_correct += predicted.eq(targets).sum().item()
 
             if batch_idx % 10 == 0:
